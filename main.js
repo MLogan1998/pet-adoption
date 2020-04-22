8105155e7333dfdadcdfcae15fcdc3a4724c3d7a
Matt Logan <mlogan5212@gmail.com>
2020-04-22 11:01:11 -0500

    created pets object

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log("Pet Adoption Setup");
+const pets = [
+  {
+    image: 'https://i.postimg.cc/kDrXqHK4/lincolnfinal.jpg',
+    name: 'Lincoln',
+    color: 'Green',
+    specialSkill: 'Coding',
+    typeOfPet: 'Lizard'
+  },
+  {
+    image: 'https://i.postimg.cc/mDk1X0T1/larryfinal.jpg',
+    name: 'Larry',
+    color: 'Blue',
+    specialSkill: 'Catching Flies',
+    typeOfPet: 'Lizard'
+  },
+  {
+    image: 'https://i.postimg.cc/x8xntyv0/hairyfinal.jpg',
+    name: 'Hairy',
+    color: 'Tan',
+    specialSkill: 'Gardening',
+    typeOfPet: 'Hamster'
+  },
+  {
+    image: 'https://i.postimg.cc/xjKsM104/hattiefinal.jpg',
+    name: 'Hattie',
+    color: 'White',
+    specialSkill: 'Relaxing',
+    typeOfPet: 'Hamster'
+  },
+  {
+    image: 'https://i.postimg.cc/cCVHZGHK/feliciafinal.jpg',
+    name: 'Felicia',
+    color: 'Yellow',
+    specialSkill: 'Blowing Bubbles',
+    typeOfPet: 'Fish'
+  },
+  {
+    image: 'https://i.postimg.cc/qMWGzNYY/frankiefinal.jpg',
+    name: 'Frankie',
+    color: 'Purple',
+    specialSkill: 'Treasure Hunting',
+    typeOfPet: 'Fish'
+  },
+]
